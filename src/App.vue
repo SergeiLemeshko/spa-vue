64767ce07a731685eea609053c5e00e02400b708
Сергей <lemexaxa@gmail.com>
2022-12-03 15:56:59 +0300

    create spa vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+	<div class="employee">
+		<div><strong>Имя:</strong>1</div>
+		<div><strong>З/П:</strong>2</div>
+		<div><strong>Возраст:</strong>3</div>
+	</div>
 </template>
 
+<script>
+export default {
+	data() {
+		return {
+
+		}
+	},
+	methods: {
+
+	}
+}
+
+</script>
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.employee {
+	padding: 15px;
+	border: 2px solid rgb(21, 64, 204);
 }
 </style>
